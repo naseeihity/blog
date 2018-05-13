13f6582f5de9bc5abbb95f2042cf130e682caa95
naseeihity <naseeihility@163.com>
2018-05-13 20:37:03 +0800

    add back to top btn

--- frontend/component/Wapper/Wapper.js
+++ frontend/component/Wapper/Wapper.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Container from './Container';
 import Nav from '../Header/Nav';
 import FootInfo from '../Footer/Footer';
+import BackToTop from '../utils/backToTop/BackToTop';
 
 const Wapper = () => (
   <div>
     <Nav />
     <Container />
     <FootInfo />
+    <BackToTop />
   </div>
 );
 
