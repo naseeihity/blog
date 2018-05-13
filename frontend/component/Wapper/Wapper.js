c273310af7953f7db53314f3acd3f40b5719dbe1
naseeihity <naseeihility@163.com>
2018-05-13 14:35:14 +0800

    add footer

--- frontend/component/Wapper/Wapper.js
+++ frontend/component/Wapper/Wapper.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Container from './Container';
 import Nav from '../Header/Nav';
+import FootInfo from '../Footer/Footer';
 
 const Wapper = () => (
   <div>
     <Nav />
     <Container />
+    <FootInfo />
   </div>
 );
 
